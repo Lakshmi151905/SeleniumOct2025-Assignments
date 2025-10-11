3401a8f0456266a8ba6149d7c17ea8d2cc537a08
Lakshmi151905 <100837049+Lakshmi151905@users.noreply.github.com>
2025-10-11 19:24:04 +0100

    Add files via upload
    Week 1 Day 1 Assignments

---
+++ week1/day1Assignments/Employee.java
@@ -0,0 +1,24 @@
+package week1.day1Assignments;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		String name = "John Doe";
+		byte age = 30;
+		double salary = 55000.75;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210L;
+		int pinCode = 56001;
+		byte luckyNumber = 7;
+		
+		System.out.println("Employee Details are displayed below \n Employee's Name : " +name+ 
+				"\n Employee's Age :" +age+ "\n Employee's Salary : " +salary+
+				"\n Employee's Gender : " +gender+ "\n Employee's Accepts Terms of the Organization :"+acceptTerms+
+				"\n Employee's Rating : " +rating+ "\n Employee's Mobile Number : " +mobileNumber+
+		      "\n Employee's Pin Code : " +pinCode+ "\n Employee's Lucky Number : " +luckyNumber );
+	
+	}
+
+}
